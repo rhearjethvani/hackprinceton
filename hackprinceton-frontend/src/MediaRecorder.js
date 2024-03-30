c58c2acf45569e890f2fef5241633bb388b5f9c1
Rhea Jethvani <rjethvan@uci.edu>
2024-03-30 03:15:49 -0400

    Webcam Integration

--- hackprinceton-frontend/src/MediaRecorder.js
+++ hackprinceton-frontend/src/MediaRecorder.js
@@ -1,9 +1,11 @@
 import React, { useState, useRef } from 'react';
+import Webcam from "react-webcam";
 
 const MediaRecorderComponent = () => {
   return (
     <div>
       <p>Media Recorder Hello World!</p>
+      <Webcam />
     </div>
   );
 };
